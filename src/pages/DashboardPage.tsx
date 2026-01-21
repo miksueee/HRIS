d5bfaeef53a30bc0abe97e88d735eac5fd5d150a
miksueee <mdenisecobarrubias@gmail.com>
2026-01-21 17:36:39 +0800

    HR DASHBOARD

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -4,16 +4,18 @@ import ManagerDashboard from './dashboards/ManagerDashboard';
 import EmployeeDashboard from './dashboards/EmployeeDashboard';
 
 export default function DashboardPage() {
-  const { user } = useAuth();
 
-  switch (user?.role) {
-    case 'hr':
-      return <HRDashboard />;
-    case 'manager':
-      return <ManagerDashboard />;
-    case 'employee':
-      return <EmployeeDashboard />;
-    default:
-      return <EmployeeDashboard />;
-  }
+  return <HRDashboard />;
+  // const { user } = useAuth();
+
+  // switch (user?.role) {
+  //   case 'hr':
+  //     return <HRDashboard />;
+  //   case 'manager':
+  //     return <ManagerDashboard />;
+  //   case 'employee':
+  //     return <EmployeeDashboard />;
+  //   default:
+  //     return <EmployeeDashboard />;
+  // }
 }
